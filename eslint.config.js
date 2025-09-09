39f655c169db802f2f144727c932c7b3f458912f
horetsky <nadvorniyoleh@icloud.com>
2025-09-09 17:36:18 +0300

    Configure ESLint with @stylistic/eslint-plugin.

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -19,5 +20,24 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
+    plugins: {
+      "@stylistic": stylistic
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "off",
+      "no-empty-pattern": "warn",
+      "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^_" }],
+      "@typescript-eslint/no-empty-object-type": "warn",
+
+      // Stylistic rules
+      "@stylistic/indent": ["error", 4],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/comma-dangle": ["error", "always-multiline"],
+      "@stylistic/space-before-function-paren": ["error", "never"]
+    }
+  }
 ])
